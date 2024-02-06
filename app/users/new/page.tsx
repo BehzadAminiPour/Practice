8865ba8355a2f212ae7552204db03b29c217f1df
behzad <behzadaminipour87@gmail.com>
2024-02-06 20:02:04 +0330

    Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,19 @@
-
+"use client";
+import { useRouter } from "next/navigation";
 
 const NewUser = () => {
+  const router = useRouter();
   return (
-    <div>NewUser</div>
-  )
-}
+    <div>
+      <h1>NewUser</h1>
+      <button
+        onClick={() => router.push("/users")}
+        className="btn btn-sm mt-2 btn-secondary"
+      >
+        Back to Users
+      </button>
+    </div>
+  );
+};
 
-export default NewUser
+export default NewUser;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -10,6 +10,9 @@ const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <div>
       <h2>UserPage</h2>
+      <Link className="btn btn-primary btn-sm my-2" href="/users/new">
+        New User
+      </Link>
       <p>{new Date().toLocaleTimeString()}</p>
       <AddNewUser text="Add New" />
       <UserTable sortOrder={sortOrder} />
